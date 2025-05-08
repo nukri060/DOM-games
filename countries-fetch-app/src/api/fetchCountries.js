3904c98a9bfb368e1115e08ded8e0f337fc471b1
nukri060 <nukri060@gmail.com>
2025-05-09 00:10:39 +0400

    feat: implement data fetch from REST Countries API

---
+++ countries-fetch-app/src/api/fetchCountries.js
@@ -0,0 +1,16 @@
+async function getData() {
+    const url = "https://restcountries.com/v3.1/all";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+getData(); 
